9efb2053298f07b9c61451860f157bc2c08a5e8e
Bianca Antoci <bianca.antoci96@gmail.com>
2020-10-31 18:54:45 +0000

    First commit

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,30 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Courses',
+    icon: 'home-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'Login or Register',
+    icon: 'home-outline',
+    link: '/pages/dashboard',
+  },
+  {
+    title: 'Admin',
+    icon: 'home-outline',
+    link: '/pages/dashboard',
+  },
+  {
+    title: 'Contact us',
+    icon: 'home-outline',
+    link: '/pages/dashboard',
+  },
+  {
+    title: 'Help',
+    icon: 'home-outline',
+    link: '/pages/dashboard',
+  },
+];
