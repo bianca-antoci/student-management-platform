168595c70ed92729c54cb9d00c909f160aee2487
Bianca Antoci <bianca.antoci96@gmail.com>
2020-11-07 10:34:21 +0000

    update login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,26 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'ngx-login',
-  styleUrls: ['login.component.scss'],
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-  courses = [
-    {
-      title: 'CertHE in Skills',
-      desc: 'this is a test',
-    },
-    {
-      title: 'Health and Social Care',
-      desc: 'this is a test',
-    },
-  ];
+export class LoginComponent implements OnInit {
+  signForm: FormGroup;
+  submitted = false;
+  valid: any;
+  showMessages = {
+    error: false,
+    success: false,
+  };
 
-  constructor() {
+  errors = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    this.signForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+
+    this.route.queryParams.subscribe((params) => {
+      if (params['message'] === 'true') {
+        this.errors.push(
+          'For security reasons we want to make sure that is really you',
+        );
+        this.errors.push('Please login with your email and password');
+        this.showMessages.error = true;
+      }
+    });
+
+    const email = localStorage.getItem('email');
+    if (email) {
+      this.formRef['email'].setValue(email);
+    }
 
   }
 
-  onJoinClick(course) {
+  get formRef() {
+    return this.signForm.controls;
+  }
+
+  signIn() {
+    this.submitted = true;
+
+    if (this.signForm.invalid) {
+      return;
+    }
+
+    this.showMessages.error = false;
+    this.errors = [];
+
+    // const email = this.signForm.get('email').value;
+    // const pass = this.signForm.get('password').value;
   }
 }
