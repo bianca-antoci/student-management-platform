d0233166433919431fe6c8f8dfb6b977df48b5d3
Bianca Antoci <bianca.antoci96@gmail.com>
2020-11-28 19:53:48 +0000

    updated footer and readme.md

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 To deploy to the cloud in Firebase:
 1. Run 'ng build --prod'
 2. Copy all files from 'dist' to 'deployment/public'
-3. Run 'firebase deploy'
+3. Run 'ls'
+4. Run 'cd deployment'
+5. Run 'firebase deploy'
--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   template: `
     <span class="created-by">Created by <b><a href="https://bianca.antoci.com" target="_blank">Bianca Antoci</a></b> 2020</span>
     <div class="socials">
-      <a href="https://github.com/Bia1996" target="_blank" class="ion ion-social-github"></a>
+      <a href="https://github.com/bianca-antoci" target="_blank" class="ion ion-social-github"></a>
       <a href="https://www.facebook.com/bianca.antoci/" target="_blank" class="ion ion-social-facebook"></a>
       <a href="https://www.linkedin.com/in/bianca-antoci-61243619a/" target="_blank" class="ion ion-social-linkedin"></a>
     </div>
